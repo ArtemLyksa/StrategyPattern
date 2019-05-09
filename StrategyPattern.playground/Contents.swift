4600548e6e892d27c70b4554b9ccb934970db987
Artem <artemlyksa@gmail.com>
2019-05-09 11:58:50 +0300

    Implemented base strategy pattern

--- StrategyPattern.playground/Contents.swift
+++ StrategyPattern.playground/Contents.swift
@@ -1,1 +1,68 @@
+import UIKit
 
+// Before
+
+//struct Logger {
+//
+//    enum LogStyle {
+//        case lowercase
+//        case uppercase
+//        case capitalized
+//    }
+//
+//    let style: LogStyle
+//
+//    func log(_ message: String) {
+//        switch style {
+//        case .lowercase:
+//            print(message.lowercased())
+//        case .uppercase:
+//            print(message.uppercased())
+//        case .capitalized:
+//            print(message.capitalized)
+//        }
+//    }
+//}
+
+
+//After
+
+// What
+protocol LoggerStrategy {
+    func log(_ message: String)
+}
+
+// Who
+struct Logger {
+    let strategy: LoggerStrategy
+    
+    func log(_ message: String) {
+        strategy.log(message)
+    }
+}
+
+// How
+struct LowercaseStrategy: LoggerStrategy {
+    func log(_ message: String) {
+        print(message.lowercased())
+    }
+}
+
+struct UppercaseStrategy: LoggerStrategy {
+    func log(_ message: String) {
+        print(message.uppercased())
+    }
+}
+
+struct CapitalizedStrategy: LoggerStrategy {
+    func log(_ message: String) {
+        print(message.capitalized)
+    }
+}
+
+var logger = Logger(strategy: CapitalizedStrategy())
+logger.log("my first strategy")  // My First Strategy
+logger = Logger(strategy: UppercaseStrategy())
+logger.log("my first strategy")  // MY FIRST STRATEGY
+logger = Logger(strategy: LowercaseStrategy())
+logger.log("my first strategy")  // my first strategy
